87dabc42304ac868e729f977b5d88b7804d37eef
Arcadiy Ivanov (via Travis CI) <arcadiy@ivanov.biz>
2020-04-07 05:52:11 +0000

    Deploy pybuilder/pybuilder.github.io to github.com/pybuilder/pybuilder.github.io.git:master

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eEu
-
-bundle exec jekyll build
-bundle exec htmlproofer --allow-hash-href \
-                        --http-status-ignore=429,999 \
-                        --check-html \
-                        --check-opengraph \
-                        --check-favicon \
-                        ./_site
