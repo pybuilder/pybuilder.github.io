c2dc35b00434cba21cd0c7e00362121cf12cb33f
Arcadiy Ivanov <arcadiy@ivanov.biz>
2023-08-14 20:45:51 -0400

    Add SEO Update Twitter handle

--- build.sh
+++ build.sh
@@ -3,11 +3,13 @@
 bundle exec jekyll build
 
 bundle exec htmlproofer --allow-hash-href \
-                        --http-status-ignore=429,999 \
-                        --check-html \
-                        --check-opengraph \
-                        --check-favicon \
-                        --assume-extension \
-                        ./_site
+                        --ignore-status-codes=429,999,302 \
+                        --no-check-internal-hash \
+                        --no-check-external-hash \
+                        --no-enforce-https \
+                        --checks Links,Scripts,Images,OpenGraph,Favicon \
+                        --typhoeus='{"followlocation": true, "connecttimeout": 20, "timeout": 60}' \
+                        --hydra='{"max_concurrency": 3}' \
+                        _site
 
 bundle exec jekyll build -c _config.yml,_config-prod.yml
